f4cba089d5e064bc39fa72ebd122b8a611d2dea7
missiona.carla@gmail.com <missiona.carla@gmail.com>
2020-05-22 03:20:50 +0000

    Urls

--- conduit/urls.py
+++ conduit/urls.py
@@ -18,8 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-
-    url(r'^api/', include('conduit.apps.articles.urls', namespace='articles')),
-    url(r'^api/', include('conduit.apps.authentication.urls', namespace='authentication')),
-    url(r'^api/', include('conduit.apps.profiles.urls', namespace='profiles')),
+    
 ]
