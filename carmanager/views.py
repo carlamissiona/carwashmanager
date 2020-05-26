3bb75f2e380660b025cd0473fb9d99b1f407db7a
missiona.carla@gmail.com <missiona.carla@gmail.com>
2020-05-26 07:46:29 +0000

    New App

--- carmanager/views.py
+++ carmanager/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+def index(request):
+    # <view logic>
+    return render(request, 'index.html', {})
